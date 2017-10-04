0d596a52ec84c54afa78b1cafc22c4476632bd60
Sapna Chandiramani <schandiramani@mayurs-mbp>
2017-10-04 16:02:32 -0700

    Initial Commit

---
+++ RGB Color Slider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RGB Color Slider
+//
+//  Created by Sapna Chandiramani on 10/4/17.
+//  Copyright © 2017 Sapna Chandiramani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
